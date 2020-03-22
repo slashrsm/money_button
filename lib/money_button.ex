cedbf8b20a1dc117ec9855efa23742d68482e2e4
Janez Urevc <janez@janezurevc.name>
2020-03-22 16:43:46 +0100

    Initial commit - create empty project.

---
+++ lib/money_button.ex
@@ -0,0 +1,18 @@
+defmodule MoneyButton do
+  @moduledoc """
+  Documentation for `MoneyButton`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MoneyButton.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/money_button_test.exs
@@ -0,0 +1,8 @@
+defmodule MoneyButtonTest do
+  use ExUnit.Case
+  doctest MoneyButton
+
+  test "greets the world" do
+    assert MoneyButton.hello() == :world
+  end
+end
